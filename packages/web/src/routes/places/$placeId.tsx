6c2cc5f2aa92e5948b487f02a9ba8940f833c711
kawataro94 <heneix.s@gmail.com>
2024-11-10 11:57:05 +0900

    use search params

--- packages/web/src/routes/places/$placeId.tsx
+++ packages/web/src/routes/places/$placeId.tsx
@@ -1,11 +1,27 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { fallback, zodSearchValidator } from "@tanstack/router-zod-adapter";
+import { z } from "zod";
+
+const searchSchema = z.object({
+  view: fallback(z.enum(["list", "gallery"]), "list").default("list"),
+});
 
 export const Route = createFileRoute("/places/$placeId")({
   component: RouteComponent,
+  validateSearch: zodSearchValidator(searchSchema),
 });
 
 function RouteComponent() {
   const { placeId } = Route.useParams();
+  const { view } = Route.useSearch();
 
-  return `Hello places/${placeId} !`;
+  return (
+    <>
+      Hello
+      <br />
+      placeId: {placeId}
+      <br />
+      view: {view}
+    </>
+  );
 }
